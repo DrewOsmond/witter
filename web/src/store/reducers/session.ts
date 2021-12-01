5069f7dc38a48f1608e712208aa39ceeaa8bb769
drewosmond <drewosmond@gmail.com>
2021-12-01 11:43:48 -0800

    updated redux for login crud

---
+++ web/src/store/reducers/session.ts
@@ -0,0 +1,68 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+import { UserSession } from "../../types";
+
+export const registerUser = createAsyncThunk(
+  "session/register",
+  async (credentials) => {
+    const { data } = await axios.post("/api/session/register", credentials);
+    return data;
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "session/login",
+  async (credentials) => {
+    const { data } = await axios.post("/api/session/login", credentials);
+    return data;
+  }
+);
+
+export const restoreUser = createAsyncThunk("session/restore", async () => {
+  const { data } = await axios.post("/api/session/restore");
+  return data;
+});
+
+export const logoutUser = createAsyncThunk("session/logout", async () => {
+  await axios.delete("/api/session/logout");
+  return null;
+});
+
+const sessionSlice = createSlice({
+  name: "session",
+  initialState: { user: null, status: null } as UserSession,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(registerUser.fulfilled, (state, action) => {
+      state.status = "success";
+      state.user = action.payload;
+    });
+
+    builder.addCase(registerUser.rejected, (state, _action) => {
+      state.status = "username or email already taken";
+      state.user = null;
+    });
+
+    builder.addCase(restoreUser.fulfilled, (state, action) => {
+      state.status = "success";
+      state.user = action.payload;
+    });
+
+    builder.addCase(restoreUser.rejected, (state, _action) => {
+      state.status = "not logged in";
+      state.user = null;
+    });
+
+    builder.addCase(logoutUser.fulfilled, (state, _action) => {
+      state.status = "success";
+      state.user = null;
+    });
+
+    builder.addCase(logoutUser.rejected, (state, _action) => {
+      state.status = "something went wrong, please try again.";
+    });
+  },
+});
+
+export default sessionSlice.reducer;
---
+++ web/src/types.ts
@@ -0,0 +1,12 @@
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  picture?: string | null;
+}
+
+export interface UserSession {
+  user: User | null;
+  status: string | null;
+}

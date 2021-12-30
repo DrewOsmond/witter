eacfadd442348db75938c01807bebc1e3f156f85
drewosmond <drewosmond@gmail.com>
2021-12-29 23:35:15 -0800

    updated how I was sending path

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,4 +1,5 @@
-import { Router } from "express";
+import express, { Router, Request, Response } from "express";
+import path from "path";
 import userRouter from "./user";
 import witRouter from "./wits";
 import replyRouter from "./reply";
@@ -8,5 +9,24 @@ const router = Router();
 router.use("/session", userRouter);
 router.use("/wit", witRouter);
 router.use("/reply", replyRouter);
+
+if (process.env.NODE_ENV === "production") {
+  const sendStatic = (req: Request, res: Response) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+
+    res.sendFile(
+      path.resolve(__dirname, "../../../web", "build", "index.html")
+    );
+  };
+
+  router.use(
+    express.static(
+      path.resolve(__dirname, "../../../web", "build", "index.html")
+    )
+  );
+
+  router.get("/", sendStatic);
+  router.get(/^(?!\/?api).*/, sendStatic);
+}
 
 export default router;
